b6eea0e5088561f57f66a539a84e8610c5e9c34e
Yuri Freire <yuriifreire@gmail.com>
2021-02-04 11:27:49 -0300

    Add frontend with React and configure axios-proxy for consumes django-api

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+// frontend/src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';         // add this
+import './index.css';
+import App from './App';
+//import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
